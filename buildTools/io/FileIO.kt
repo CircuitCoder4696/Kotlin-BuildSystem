a0ee562d00bf0d825cc94c38f3afa12694d6748c
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-10-03 00:44:41 +0000

    :/

---
+++ buildTools/io/FileIO.kt
@@ -0,0 +1,9 @@
+package buildTools.io
+
+class FileIO {
+    companion object {
+        fun readLinesAsStrings(path:String):Array<String> {
+            var f= Paths.get(path);
+        }
+    }
+}
